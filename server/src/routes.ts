3fac91991482edcc30ab2b5d978172bfb14d28cf
marcos012 <marcospaulo.ad@gmail.com>
2020-06-04 01:33:59 -0300

    adiciona index para pontos de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,7 +7,9 @@ const pointsController = new PointsControlles();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
+
 routes.post('/points', pointsController.create)
-routes.get('/points/:id', itemsController.show);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
