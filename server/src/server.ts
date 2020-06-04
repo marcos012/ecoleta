0efa36f613931d2fc542d8433930b7d67e5ebb4f
marcos012 <marcospaulo.ad@gmail.com>
2020-06-03 23:34:44 -0300

    estrutura banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/user', (req, res) => {
-    res.json({'Marcos': 's', 'sssss': 'a'});
-    
-});
+app.use(express.json());
+
+app.use(routes)
 
 app.listen(3333);
