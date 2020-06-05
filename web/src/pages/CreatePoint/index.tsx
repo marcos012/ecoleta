4913ea2dca0a42ffe82d2936b93f8e6a75f3ccb3
marcos012 <marcospaulo.ad@gmail.com>
2020-06-05 01:23:55 -0300

    buscando itens do backend

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,27 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker } from "react-leaflet";
+import api from "../../services/api";
 
 import "./CreatePoint.css";
 import logo from '../../assets/logo.svg';
 
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
 const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(res => {
+            setItems(res.data);
+        });
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -78,30 +93,13 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" />
-                            <span>Óleo de cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+
                     </ul>
                 </fieldset>
 
