e61ab4230a2fbed0daef8c51242020b7b4dcea1a
marcos012 <marcospaulo.ad@gmail.com>
2020-06-06 22:00:26 -0300

    upload de arquivos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,21 @@
 import express from 'express';
+
+import multer from 'multer';
+import multerConfig from "./config/multer";
+
 import PointsControlles from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsControlles();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-
-routes.post('/points', pointsController.create)
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+
+routes.post('/points', upload.single('image'), pointsController.create)
 
 export default routes;
